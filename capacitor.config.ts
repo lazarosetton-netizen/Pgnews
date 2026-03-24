2d332cfebd7d4a5398241e87d3db09c7f32a8f8d
lazarosetton-netizen <lazarosetton@gmail.com>
2026-03-24 15:16:01 +0200

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pgnews.app',
+  appName: 'PGNEWS',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
